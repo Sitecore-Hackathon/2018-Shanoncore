04c3750fa22adbbc67ac002816dedb51cf2dbdc8
Peter Franceschini <peter.franceschini@verndale.com>
2018-03-03 10:43:44 -0500

    Renamed project, stubbed out methods

---
+++ Project.Hackathon.Website/Services/HeatmapService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project.Heatmap.Services
+{
+    public class HeatmapService
+    {
+        public XConnectService XConnectService { get; set; }
+        public HeatmapService()
+        {
+            XConnectService = new XConnectService();
+        }
+
+        // Write interaction
+
+        // read interactions with filtering
+
+
+    }
+}
