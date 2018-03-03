ccbc1ff6f764fe2ec8e3bbb052893f0b8352b241
Shanon Place <Shanon.Place@verndale.com>
2018-03-02 23:08:05 -0500

    front-end - giving up on the facny build system as the linting was killing me

--- src/fee-prototype/gulp/bootstrap.js
+++
@@ -1,5 +0,0 @@
-export default (gulp, tasks, $, config) => {
-  tasks.forEach(name => {
-    require('./tasks/' + name).default(gulp, $, config);
-  });
-};
--- src/fee-prototype/src/js/main.js
+++ src/fee-prototype/src/js/main.js
@@ -1,10 +1,5 @@
 import heatmap from 'heatmap.js';
 
-console.log(heatmap);
-
-//Hot module replacement
-if (__DEV__){
-  const config = require('../../config');
-
-  if (config.hmr && module.hot) module.hot.accept();
-}
+document.addEventListener('click', function(event){
+  console.log(event.detail)
+}, false);
