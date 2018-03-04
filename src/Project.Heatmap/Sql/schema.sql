6f6f77ed1dca15c57d3d4cd16fb3fa17c9b50f40
Peter Franceschini <peter.franceschini@verndale.com>
2018-03-03 19:22:18 -0500

    integrating js files and sitecore files into project

---
+++ src/Project.Heatmap/Sql/schema.sql
@@ -0,0 +1,9 @@
+﻿create table HeatmapDatapoint
+(
+	Id int Identity(1,1) primary key not null,
+    DateTime DateTime not null,
+	RelativeUrl nvarchar(1000),
+	X int not null,
+    Y int not null,
+    Width int not null,
+)
